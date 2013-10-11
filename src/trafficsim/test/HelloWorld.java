e67bed407b215098257c08385a0e98ee1de5ac78
Boebu <boebu@mka.ch>
2013-10-11 13:35:44 +0200

    Intial Commit

---
+++ src/trafficsim/test/HelloWorld.java
@@ -0,0 +1,15 @@
+package trafficsim.test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: boebu
+ * Date: 10/11/13
+ * Time: 1:31 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+
+}
